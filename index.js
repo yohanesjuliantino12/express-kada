0bb9a599dd1694d4fc872df39b66dbdd66d64726
yohanesjuliantino12 <yohanesjuliantino12@gmail.com>
2026-02-24 14:58:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+
+const app = express()
+
+
+app.use((req,res,next) =>{
+    if(false){
+        next(new Error('Not Authorized'))
+        return;
+    }
+    next();
+})
+
+app.get('/', (req, res, next) => {
+    res.send('Hello, Yohanes!!')
+})
+
+app.use((err, req, res, next) => {
+    res.send('Error Handling');
+});
+
+app.use((req, res, next) => {
+    console.log(`Request lewatt jalan ini ${req.path}`);
+    next();
+})
+
+app.get('/say:greeting', (req,res) => {
+    const { greeting } = req.params;
+    res.send(greeting);
+})
+
+app.listen(3000);
+
