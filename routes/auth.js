2e18b038669964d9767ef8005ff69821553e995a
yohanesjuliantino12 <yohanesjuliantino12@gmail.com>
2026-03-04 10:29:43 +0700

    ada smtp

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post("/register", async (req, res, next) => {
       { expiresIn: "1d" }
     );
 
-    const verificationLink = `http://yohaneshtgl.my.id/auth/verify/${verificationToken}`;
+    const verificationLink = `https://h5368w.csb.app/auth/verify/${verificationToken}`;
 
     // 5️⃣ Buat transporter SETELAH env siap
     const transporter = createTransporter();
