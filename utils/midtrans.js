aad300d1d1bf289efdbcbfb1f1eed9517b34ae6f
yohanesjuliantino12 <yohanesjuliantino12@gmail.com>
2026-03-04 16:02:12 +0700

    buat payment gateway

--- utils/midtrans.js
+++ utils/midtrans.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const snap = new midtransClient.Snap({
-    isProduction: false,
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION,
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.MIDTRANS_CLIENT_KEY
 });
