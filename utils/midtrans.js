64b2547ef6fd7697db3e353bad594325b7c3e401
yohanesjuliantino12 <yohanesjuliantino12@gmail.com>
2026-03-05 15:42:16 +0700

    buat payment gateway dan notifikasi

--- utils/midtrans.js
+++ utils/midtrans.js
@@ -8,11 +8,11 @@ const snap = new midtransClient.Snap({
     clientKey: process.env.MIDTRANS_CLIENT_KEY
 });
 
-// const coreAPI = new midtransClient.CoreApi({
-//     isProduction: false,
-//     serverKey: process.env.MIDTRANS_SERVER_KEY,
-//     clientKey: process.env.MIDTRANS_CLIENT_KEY
-// })
+const coreAPI = new midtransClient.CoreApi({
+    isProduction: false,
+    serverKey: process.env.MIDTRANS_SERVER_KEY,
+    clientKey: process.env.MIDTRANS_CLIENT_KEY
+})
 
 
 export const createTransaction = async (req,res) => {
@@ -44,5 +44,61 @@ export const createTransaction = async (req,res) => {
     }   catch (error){
         console.error("Error createTransaction:", error);
         res.status(500).json({ message: "Gagal membuat transaksi" });
+    }
+}
+
+export const handleNotification = async (req, res) => {
+    try {
+        const notification = req.body;
+
+
+        const statusResponse = await coreAPI.transaction.notification(notification);
+
+        const orderID = statusResponse.order_id;
+        const transactionStatus = statusResponse.transaction_status
+        const fraudStatus = statusResponse.fraud_status;
+        const paymentType = statusResponse.payment_type;
+
+        console.log("Order ID:", orderID);
+        console.log("Transaction Status:", transactionStatus);
+
+        if  (transactionStatus === "capture") {
+            if(fraudStatus === "accept") {
+                console.log("Pembayaran berhasil (capture)")
+            }
+        }   else if (transactionStatus === "settlement") {
+            console.log("Pembayaran berhasil (settlement)");
+        }   else if (transactionStatus === "pending") {
+            console.log("Menunggu pembayaran");
+        }   else if (
+            transactionStatus === "cancel" ||
+            transactionStatus === "deny" ||
+            transactionStatus === "expire"
+           )   {
+            console.log("Pembayaran gagal");
+        }
+        res.status (200).json({ message: "Notification handled", status: statusResponse});
+
+    } catch (error) {
+        console.error("Error handleNotification", error);
+        res.status(500).json({ message: "Error handling notification"});
+    }
+};
+
+export const checkStatus = async (req, res) => {
+    try {
+        const { orderID } = req.params;
+
+        const statusResponse = await coreAPI.transaction.status(orderID);
+
+        res.status(200).json({
+            order_id: statusResponse.order_id,
+            transaction_status: statusResponse.transaction_status,
+            payment_type: statusResponse.payment_type,
+            fraud_status: statusResponse.fraud_status
+        });
+    } catch (error) {
+        console.error("Error checkStatus:", error);
+        res.statusS(500).json({ message: "Gagal cek status transaksi"});
     }
 }
