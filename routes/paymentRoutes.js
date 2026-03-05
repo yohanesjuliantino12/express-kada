55ebb0dcb26a5b238bd7ac4c773971e2a244eeab
yohanesjuliantino12 <yohanesjuliantino12@gmail.com>
2026-03-05 15:57:35 +0700

    buat payment gateway dan notifikasi

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createTransaction } from "../utils/midtrans.js";
+import { createTransaction, handleNotification, checkStatus } from "../utils/midtrans.js";
 
 const router = express.Router();
 
 router.post("/create", createTransaction);
+router.post("/notification", handleNotification, checkStatus)
 
 export default router;
